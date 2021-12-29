e48052a1f6fd1049ca1993e7ee218c859cdb9f70
jxstxn1 <justin@phntm.xyz>
2021-12-29 11:03:37 +0100

    SetUp

--- packages/ttt_sidekick/bin/ttt.dart
+++
@@ -1,5 +0,0 @@
-import 'package:ttt_sidekick/ttt_sidekick.dart';
-
-Future<void> main(List<String> arguments) async {
-  await TttSidekick().runWithArgs(arguments);
-}
--- packages/ttt_sidekick/lib/src/ttt_command_runner.dart
+++
@@ -1,6 +0,0 @@
-import 'package:args/command_runner.dart';
-
-class TttCommandRunner extends CommandRunner {
-  TttCommandRunner()
-      : super('Sidekick', 'A convenience script that simplifies setting up and running your project project.');
-}
